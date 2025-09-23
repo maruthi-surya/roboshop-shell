d1751a476dc01e7352eda71aab19528c31eb2ee1
maruthi-surya <maruthi.surya.k@gmail.com>
2025-09-23 20:47:57 +0530

    comment

---
+++ instances.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ami_id="ami-09c813fb71547fc4f"
+sg_id="sg-02704cfa1a0558868"
+
+for instance in $@
+do
+
+instance_id=$(aws ec2 run-instances --image-id $ami_id --instance-type t3.micro --security-group-ids $sg_id --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+
+if[ $instance != forntend ]; then
+
+ip=$(ec2 describe-instances --instance-ids $instance 'Reservations[0].Instances[0].[PrivateIpAddress]' --output text)
+
+else
+
+ip=$(aws ec2 describe-instances --instance-ids $instance 'Reservations[0].Instances[0].[PublicIpAddress]' --output text)
+
+fi
+
+echo "$instance:$ip"
+
+done
