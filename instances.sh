a9bcb40ad3d00dd3e13f88ffcb1e6ad36e83e7a6
maruthi-surya <maruthi.surya.k@gmail.com>
2025-09-23 21:26:39 +0530

    comment

--- instances.sh
+++ instances.sh
@@ -2,6 +2,7 @@
 
 ami_id="ami-09c813fb71547fc4f"
 sg_id="sg-02704cfa1a0558868"
+domain_name="daws-practice.fun"
 
 for instance in $@
 do
@@ -11,13 +12,16 @@ instance_id=$(aws ec2 run-instances --image-id $ami_id --instance-type t3.micro 
 if [ $instance != "frontend" ]; then
 
 ip=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+record_name=$instance.$domain_name
 
 else
 
 ip=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+record_name=$domain_name
 
 fi
 
 echo "$instance: $ip"
+echo "$record_name"
 
 done
