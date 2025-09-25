eb3eca2f5e2f4fb6e3b89224678ae2300d69ec15
maruthi-surya <maruthi.surya.k@gmail.com>
2025-09-25 17:40:30 +0530

    commit

--- file_name.sh
+++ file_name.sh
@@ -4,5 +4,5 @@ dir_path=$(pwd)
 file=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$dir_path/$file.log"
 echo "Directory Path: $dir_path" | tee -a $LOG_FILE
-echo "File: $file"  | tee -a $LOG_FILE
-echo "File Name: $LOG_FILE" | tee -a $LOG_FILE
+echo "File: $file"  &>>$LOG_FILE
+echo "File Name: $LOG_FILE" &>>$LOG_FILE
