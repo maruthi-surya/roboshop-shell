e120d0cfbcf8333c2622d91a9732750655d37995
maruthi-surya <maruthi.surya.k@gmail.com>
2025-09-27 10:46:21 +0530

    comment

--- file_name.sh
+++ file_name.sh
@@ -3,9 +3,11 @@
 dir_path=$(pwd)/logs #get the current working directory
 echo "dir-path: $dir_path"
 file_name=$0 #get current file name
+curr_date=$(date +%+4Y-%m-%d)
+ echo "Date: $curr_date"
  echo "File-Name: $file_name" # echo print the results on the screen
 file=$( echo $0 | cut -d "." -f1 ) # cut command used to cut the stirng based on the delimiter -d
-LOG_FILE="$dir_path/$file.log" # appending filename to the directory
+LOG_FILE="$dir_path/$file_$curr_date.log" # appending filename to the directory
 echo "Directory Path: $dir_path" | tee -a $LOG_FILE # tee command writes into the file also display
 echo "File: $file"  &>>$LOG_FILE
 echo "File Name: $LOG_FILE" &>>$LOG_FILE
@@ -15,5 +17,3 @@ if [ -d $dir ]; then
 else
  echo "dircotry doesn't exist"
 fi
-curr_date=$(date +%+4Y-%m-%d)
- echo "Date: $curr_date"
