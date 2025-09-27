e9e27ae4cc9bba0e4205a1944b499656a32dd271
maruthi-surya <maruthi.surya.k@gmail.com>
2025-09-27 09:24:35 +0530

    comment

--- file_name.sh
+++ file_name.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 dir_path=$(pwd)
+file_name=$0
+ echo "File-Name: $file_name"
 file=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$dir_path/$file.log"
 echo "Directory Path: $dir_path" | tee -a $LOG_FILE
